53ecefcebbb4caf739f4900cd08d7492e455337f
rollrat <rollrat.cse@gmail.com>
2022-05-19 20:43:36 +0900

    init

---
+++ src/main/java/com/example/springstudyinha/SpringstudyinhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.springstudyinha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstudyinhaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringstudyinhaApplication.class, args);
+    }
+
+}
